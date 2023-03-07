9f0ec7eec6d5b5b51cb0fcee50d33e7a70f8a6d7
Goh Chok Yao <chokyao@gmail.com>
2023-03-07 11:36:38 +0800

    Add delete function

---
+++ src/main/java/seedu/modules/ModuleList.java
@@ -0,0 +1,30 @@
+package seedu.modules;
+import java.util.ArrayList;
+
+public class ModuleList {
+    private ArrayList<Module> listOfModules;
+    public ModuleList() {
+        this.listOfModules = new ArrayList<>();
+    }
+    public ArrayList<Module> getModuleList() {
+        return listOfModules;
+    }
+
+    public void findModule(String moduleCode) {
+        for (int i = 0; i < listOfModules.size(); i++) {
+            if (listOfModules.get(i).getModuleCode().equals(moduleCode)) {
+                //Print.printFoundModuleMessage();
+                break;
+            }
+        }
+    }
+
+    public void deleteModule(String moduleCode) {
+        for (int i = 0; i < listOfModules.size(); i++) {
+            if (listOfModules.get(i).getModuleCode().equals(moduleCode)) {
+                listOfModules.remove(i);
+            }
+        }
+    }
+
+}
